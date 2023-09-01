7b2e5269afd7696aecacc6d836c3388cc80d5883
Jasmine Mary <jasminemary630@gmail.com>
2023-09-01 16:15:24 +0530

    cache mechanism added

--- order-service/src/main/java/com/dev/orderservice/config/Config.java
+++ order-service/src/main/java/com/dev/orderservice/config/Config.java
@@ -1,4 +1,4 @@
-package com.dev.productservice.config;
+package com.dev.orderservice.config;
 
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
@@ -6,22 +6,16 @@ import org.springframework.cache.concurrent.ConcurrentMapCache;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class Config {
-
-    @Bean
-    public WebClient.Builder webClient(){
-        return WebClient.builder();
-    }
 
     @Bean
     public CacheManager cacheManager() {
         ConcurrentMapCacheManager cacheManager = new ConcurrentMapCacheManager() {
             @Override
             protected Cache createConcurrentMapCache(String name) {
-                if (name.equals("productCache")) {
+                if (name.equals("orderCache")) {
                     return new ConcurrentMapCache(name, false);
                 }
                 return super.createConcurrentMapCache(name);
--- product-service/src/main/java/com/dev/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/dev/productservice/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.dev.productservice.dto.OrderResponse;
 import com.dev.productservice.dto.ProductDto;
 import com.dev.productservice.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,24 +26,28 @@ public class ProductController {
     }
 
     @GetMapping("/all-products")
+    @Cacheable(value = "allProductsCache")
     public ResponseEntity<List<ProductDto>> getAllProducts(){
         List<ProductDto> productDtoList = productService.getAllProduct();
         return ResponseEntity.ok(productDtoList);
     }
 
     @GetMapping("/{id}")
+    @Cacheable(value = "productById", key = "#id")
     public ResponseEntity<ProductDto> getProductById(@PathVariable long id){
         ProductDto productDto = productService.getProductById(id);
         return ResponseEntity.ok(productDto);
     }
 
     @GetMapping("/{id}/order")
+    @Cacheable(value = "placeOrder", key = "#id")
     public ResponseEntity<Map<String, String>> placeOrder(@PathVariable long id){
         Map<String, String> map = productService.placeOrder(id);
         return ResponseEntity.ok(map);
     }
 
     @GetMapping("/{orderNumber}/order-item")
+    @Cacheable(value = "productByOrderNumber", key = "#orderNumber")
     public ResponseEntity<OrderResponse> getProductByOrderNUmber(@PathVariable String orderNumber){
         return ResponseEntity.ok(productService.getProductByOrderNumber(orderNumber));
     }
